c03eb9e6510bb193b2de893c53327f211074283f
Amanchess <106991909+Amanchess@users.noreply.github.com>
2024-05-26 16:52:13 +0530

    Update FrontPage.js

--- src/components/Front page.js
+++ src/components/Front page.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+ import React, { useState, useEffect } from 'react';
 import Lottie from 'react-lottie';
 import { Link } from 'react-router-dom';
 import bg from './assets/images/page-1_bg.png';
